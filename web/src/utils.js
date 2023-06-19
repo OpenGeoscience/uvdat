db5f782639749f7c3966aa449d32e7ba2c2b352f
Anne Haley <anne.haley@kitware.com>
2023-06-19 14:51:02 -0400

    Write layer creation functions in utils

--- web/src/utils.js
+++ web/src/utils.js
@@ -1,6 +1,14 @@
+import TileLayer from "ol/layer/Tile.js";
+import VectorTileLayer from "ol/layer/VectorTile.js";
+import XYZSource from "ol/source/XYZ.js";
+import VectorTileSource from "ol/source/VectorTile.js";
+import GeoJSON from "ol/format/GeoJSON.js";
 import { Fill, Stroke, Circle, Style } from "ol/style.js";
 
-export const createStyle = (args) => {
+import { baseURL } from "@/api/auth";
+import { map } from "@/store";
+
+function createStyle(args) {
   let colors = ["#00000022"];
   if (args.colors) {
     colors = args.colors.split(",");
@@ -31,4 +39,38 @@ export const createStyle = (args) => {
     }
     return new Style(styleSpec);
   });
-};
+}
+
+export function addDatasetLayerToMap(dataset) {
+  if (dataset.raster_file) {
+    map.value.addLayer(
+      new TileLayer({
+        properties: {
+          datasetId: dataset.id,
+        },
+        source: new XYZSource({
+          url: `${baseURL}datasets/${dataset.id}/tiles/{z}/{x}/{y}.png/?projection=EPSG:3857`,
+        }),
+        opacity: 0.7,
+      })
+    );
+  } else if (dataset.geodata_file) {
+    map.value.addLayer(
+      new VectorTileLayer({
+        properties: {
+          datasetId: dataset.id,
+        },
+        source: new VectorTileSource({
+          format: new GeoJSON(),
+          url: `${baseURL}datasets/${dataset.id}/vector-tiles/{z}/{x}/{y}/`,
+        }),
+        style: function (feature) {
+          return createStyle({
+            type: feature.getGeometry().getType(),
+            colors: feature.get("colors"),
+          });
+        },
+      })
+    );
+  }
+}
