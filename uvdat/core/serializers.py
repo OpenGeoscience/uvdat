e6b60bcdd4667083484a55e714ed9c1e570876e8
Anne Haley <anne.haley@kitware.com>
2023-07-11 09:05:07 -0400

    Create NetworkNode model and serializer

--- uvdat/core/serializers.py
+++ uvdat/core/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from uvdat.core.models import City, Dataset
+from uvdat.core.models import City, Dataset, NetworkNode
+
+
+class NetworkNodeSerializer(serializers.ModelSerializer):
+    location = serializers.SerializerMethodField('get_location')
+
+    def get_location(self, obj):
+        if obj.location:
+            return [obj.location.y, obj.location.x]
+        else:
+            return [0, 0]
+
+    class Meta:
+        model = NetworkNode
+        fields = '__all__'
 
 
 class DatasetSerializer(serializers.ModelSerializer):
+    network_nodes = NetworkNodeSerializer(many=True)
+
     class Meta:
         model = Dataset
         fields = '__all__'
