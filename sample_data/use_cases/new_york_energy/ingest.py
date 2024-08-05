ee550201c20bd724d02a7d736a8807d88afb001b
Anne Haley <anne.haley@kitware.com>
2024-08-05 12:54:01 +0000

    fix: remove other usages of module reference for nysdp datasets

--- sample_data/use_cases/new_york_energy/ingest.py
+++ sample_data/use_cases/new_york_energy/ingest.py
@@ -1,23 +1,27 @@
 from pathlib import Path
 from .import_networks import perform_import
 from .export_networks import perform_export
-from .nysdp import create_consolidated_network
+from .nysdp import create_consolidated_network, create_vector_features
 
 
 DOWNLOADS_FOLDER = DOWNLOADS_FOLDER = Path('../../sample_data/downloads')
 PULL_LATEST = False
 
 
-def convert_dataset(dataset, options, ):
+def convert_dataset(dataset, options):
     print('\t', f'Converting data for {dataset.name}...')
     if dataset.name == 'National Grid County Networks':
         if PULL_LATEST:
             # pull latest data from NYSDP and run network interpretation algorithm
-            dataset.source_files.delete()
+            dataset.source_files.all().delete()
             create_consolidated_network(dataset, downloads_folder=DOWNLOADS_FOLDER)
             perform_export()
         else:
             perform_import(dataset, downloads_folder=DOWNLOADS_FOLDER)
+    elif dataset.name == 'National Grid CompanyBoundary':
+        create_vector_features(dataset, 'CompanyBoundary')
+    elif dataset.name == 'National Grid Substations':
+        create_vector_features(dataset, 'Substations')
     else:
         dataset.spawn_conversion_task(
             style_options=options.get('style_options'),
