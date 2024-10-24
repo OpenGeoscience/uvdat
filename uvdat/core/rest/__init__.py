023da471df440dae2ec42da5c1f4b8a49b518628
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-10-24 15:02:25 -0400

    refactor: Remove unused endpoints

--- uvdat/core/rest/__init__.py
+++ uvdat/core/rest/__init__.py
@@ -1,8 +1,6 @@
 from .chart import ChartViewSet
 from .dataset import DatasetViewSet
-from .file_item import FileItemViewSet
 from .map_layers import RasterMapLayerViewSet, VectorMapLayerViewSet
-from .network import NetworkEdgeViewSet, NetworkNodeViewSet, NetworkViewSet
 from .project import ProjectViewSet
 from .regions import SourceRegionViewSet
 from .simulations import SimulationViewSet
@@ -11,12 +9,8 @@ from .user import UserViewSet
 __all__ = [
     ProjectViewSet,
     ChartViewSet,
-    FileItemViewSet,
     RasterMapLayerViewSet,
     VectorMapLayerViewSet,
-    NetworkViewSet,
-    NetworkNodeViewSet,
-    NetworkEdgeViewSet,
     DatasetViewSet,
     SourceRegionViewSet,
     SimulationViewSet,
