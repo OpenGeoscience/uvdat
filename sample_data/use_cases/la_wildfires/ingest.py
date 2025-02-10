415433f3458ba3cc7ac2be47443144889673b9de
Forrest <forrest.li@kitware.com>
2025-02-10 12:43:09 -0500

    Add LA wildfires as a use-case

---
+++ sample_data/use_cases/la_wildfires/ingest.py
@@ -0,0 +1,8 @@
+def convert_dataset(dataset, options):
+    print('\t', f'Converting data for {dataset.name}...')
+    dataset.spawn_conversion_task(
+        style_options=options.get('style_options'),
+        network_options=options.get('network_options'),
+        region_options=options.get('region_options'),
+        asynchronous=False,
+    )
