67aaf7325b9a9c8d04db588c5ac45d8737bb81f3
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-05-05 18:07:33 -0400

    Restrict scope to just query param token auth

--- uvdat/core/rest/tokenauth.py
+++ uvdat/core/rest/tokenauth.py
@@ -2,25 +2,21 @@
 # ipyleaflet does not support custom headers, so auth token must be sent as a URL param
 
 from django.contrib.auth.models import User
-from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from rest_framework.authentication import BaseAuthentication
 
 
-class TokenAuth(OAuth2Authentication):
+class TokenAuth(BaseAuthentication):
     def authenticate(self, request):
-        auth = super().authenticate(request)
-        if auth is not None:
-            return auth
         token = request.query_params.get('token')
         if token is None:
-            token_string = request.headers.get('Authorization')
-            token = token_string.replace('Token ', '')
-        if token is not None:
-            try:
-                user = User.objects.get(auth_token=token)
-            except User.DoesNotExist:
-                return None
+            return
 
-            if not user.is_active:
-                return None
-            return (user, None)
-        return None
+        try:
+            user = User.objects.get(auth_token=token)
+        except User.DoesNotExist:
+            return None
+
+        if not user.is_active:
+            return None
+
+        return (user, None)
