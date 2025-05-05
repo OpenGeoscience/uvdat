2966d0828b793d2ae7608b489337d86437d17dd6
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-05-05 15:17:42 -0400

    Gracefully handle missing `Authorization` header

--- uvdat/core/rest/tokenauth.py
+++ uvdat/core/rest/tokenauth.py
@@ -6,21 +6,31 @@ from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 
 
 class TokenAuth(OAuth2Authentication):
+    @staticmethod
+    def get_token_from_query(request) -> str | None:
+        token = request.query_params.get('token')
+        if token is not None:
+            return token
+
+        auth_token = request.headers.get('Authorization')
+        if auth_token is not None:
+            return auth_token.replace('Token ', '')
+
     def authenticate(self, request):
         auth = super().authenticate(request)
         if auth is not None:
             return auth
-        token = request.query_params.get('token')
-        if token is None:
-            token_string = request.headers.get('Authorization')
-            token = token_string.replace('Token ', '')
-        if token is not None:
-            try:
-                user = User.objects.get(auth_token=token)
-            except User.DoesNotExist:
-                return None
 
-            if not user.is_active:
-                return None
-            return (user, None)
-        return None
+        token = self.get_token_from_query(request)
+        if token is None:
+            return None
+
+        try:
+            user = User.objects.get(auth_token=token)
+        except User.DoesNotExist:
+            return None
+
+        if not user.is_active:
+            return None
+
+        return (user, None)
