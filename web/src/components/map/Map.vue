a35066cd6e69f1b26cfac1f40f011b8c58c8156b
Anne Haley <anne.haley@kitware.com>
2025-06-06 18:20:13 +0000

    fix: Ignore AbortErrors raised when updating raster layer styles

--- web/src/components/map/Map.vue
+++ web/src/components/map/Map.vue
@@ -114,6 +114,10 @@ function createMap() {
   newMap.on("load", () => {
     mapContainer.classList.remove("spinner");
   });
+  newMap.on('error', (response) => {
+    // AbortErrors are raised when updating style of raster layers; ignore these
+    if (response.error.message !== 'AbortError') console.error(response.error)
+});
 
   /**
    * This is called on every click, and technically hides the tooltip on every click.
