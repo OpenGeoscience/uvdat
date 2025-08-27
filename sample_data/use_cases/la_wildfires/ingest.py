a871ed0b9e16db779952bdbcdc38626d9b8e5381
Bryon Lewis <Bryon.Lewis@kitware.com>
2025-08-27 15:42:36 -0400

    feat: restructure sample_data to transfer to ingest management command

--- sample_data/use_cases/la_wildfires/ingest.py
+++
@@ -1,7 +0,0 @@
-def convert_dataset(dataset, options):
-    print('\t', f'Converting data for {dataset.name}...')
-    dataset.spawn_conversion_task(
-        network_options=options.get('network_options'),
-        region_options=options.get('region_options'),
-        asynchronous=False,
-    )
