9e37142eebbfaa82bf24990dcce5472b11a9f388
Anne Haley <anne.haley@kitware.com>
2025-09-22 11:16:18 -0400

    feat: Add schema validation to colormap `markers` field

--- uvdat/core/models/colormap.py
+++ uvdat/core/models/colormap.py
@@ -1,6 +1,27 @@
 from django.db import models
+from jsonschema import validate
 
 from .project import Project
+
+MARKER_SCHEMA = dict(
+    type='array',
+    items=dict(
+        type='object',
+        properties=dict(
+            color=dict(
+                type='string',
+            ),
+            value=dict(
+                type='number',
+                minimum=0,
+                maximum=1,
+            ),
+        ),
+        required=['color', 'value'],
+    ),
+    minItems=2,
+    uniqueItems=True,
+)
 
 
 class Colormap(models.Model):
@@ -12,3 +33,11 @@ class Colormap(models.Model):
         on_delete=models.CASCADE,
         null=True,
     )
+
+    def clean(self):
+        if len(self.markers):
+            validate(instance=self.markers, schema=MARKER_SCHEMA)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
