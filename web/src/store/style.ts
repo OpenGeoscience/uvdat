df8ac65af6d4bb6c02042d06a2d386d57c9945c0
Anne Haley <anne.haley@kitware.com>
2025-09-24 16:33:09 -0400

    fix: Apply source filters from frame definitions

--- web/src/store/style.ts
+++ web/src/store/style.ts
@@ -96,6 +96,11 @@ function getRasterTilesQuery(styleSpec: StyleSpec) {
             if (colorSpec.colormap && colorSpec.visible) query.bands.push(colorQuery)
         }
     })
+    styleSpec.filters.forEach((f) => {
+        if (f.apply && f.filter_by && f.include && f.list?.length === 1) {
+            query[f.filter_by] = f.list[0]
+        }
+    })
     if (!Object.keys(query).length) return undefined
     return query;
 }
@@ -348,6 +353,7 @@ export const useStyleStore = defineStore('style', () => {
                     list: [v],
                     include: true,
                     transparency: true,
+                    apply: true,
                 }))
             ]
         }
@@ -394,7 +400,7 @@ export const useStyleStore = defineStore('style', () => {
                 map.setPaintProperty(mapLayerId, 'circle-stroke-opacity', visibility)
             }
         } else if (mapLayerId.includes("raster")) {
-            const rasterTilesQuery = getRasterTilesQuery(styleSpec)
+            const rasterTilesQuery = getRasterTilesQuery({...styleSpec, filters})
             if (rasterTilesQuery?.bands && !rasterTilesQuery.bands.length) opacity = 0
             map.setPaintProperty(mapLayerId, "raster-opacity", opacity)
             let source = map.getSource(mapLayer.source) as RasterTileSource;
