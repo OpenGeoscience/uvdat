2759e0739e6c69390279cd117a35cf0ecdd56ab0
semantic-release <semantic-release>
2025-10-24 14:34:51 +0000

    1.14.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-__version__ = '1.13.1'
+__version__ = '1.14.0'
 
 readme_file = Path(__file__).parent / 'README.md'
 if readme_file.exists():
