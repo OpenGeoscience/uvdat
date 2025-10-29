b53107b7894b9c3570a8fbef542019fab424e433
Anne Haley <anne.haley@kitware.com>
2025-10-29 14:37:32 -0400

    fix: Update vite app env vars again

--- web/src/api/auth.js
+++ web/src/api/auth.js
@@ -11,7 +11,7 @@ export const apiClient = axios.create({
 export const oauthClient = new OauthClient(
   new URL(import.meta.env.VITE_APP_OAUTH_API_ROOT),
   import.meta.env.VITE_APP_OAUTH_CLIENT_ID,
-  { redirectUrl: import.meta.env.VITE_APP_API_ROOT }
+  { redirectUrl: import.meta.env.VITE_APP_BASE_URL }
 );
 
 let s3ffClient = undefined;
