ce42903f55a990c14e1ee173c41276601862e3dc
Brian Helba <brian.helba@kitware.com>
2025-11-19 11:21:49 -0500

    Use `@admin.register` decorator

--- geoinsight/core/admin.py
+++ geoinsight/core/admin.py
@@ -24,113 +24,110 @@ from geoinsight.core.models import (
     VectorFeature,
 )
 
-
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
 
+@admin.register(Dataset)
 class DatasetAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'category']
 
 
+@admin.register(FileItem)
 class FileItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'dataset', 'chart']
 
 
+@admin.register(Chart)
 class ChartAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'editable']
 
 
+@admin.register(Layer)
 class LayerAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'dataset']
 
 
+@admin.register(LayerFrame)
 class LayerFrameAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'index', 'layer']
 
 
+@admin.register(LayerStyle)
 class LayerStyleAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'layer']
 
 
+@admin.register(Colormap)
 class ColormapAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
 
+@admin.register(ColorConfig)
 class ColorConfigAdmin(admin.ModelAdmin):
     list_display = ['id', 'style', 'name']
 
 
+@admin.register(ColormapConfig)
 class ColormapConfigAdmin(admin.ModelAdmin):
     list_display = ['id', 'colormap', 'color_by']
 
 
+@admin.register(SizeConfig)
 class SizeConfigAdmin(admin.ModelAdmin):
     list_display = ['id', 'style', 'name']
 
 
+@admin.register(SizeRangeConfig)
 class SizeRangeConfigAdmin(admin.ModelAdmin):
     list_display = ['id', 'minimum', 'maximum', 'size_by']
 
 
+@admin.register(FilterConfig)
 class FilterConfigAdmin(admin.ModelAdmin):
     list_display = ['id', 'style', 'filter_by']
 
 
+@admin.register(RasterData)
 class RasterDataAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'source_file']
 
 
+@admin.register(VectorData)
 class VectorDataAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'source_file']
 
 
+@admin.register(VectorFeature)
 class VectorFeatureAdmin(admin.ModelAdmin):
     list_display = ['id', 'dataset']
     list_select_related = ['vector_data__dataset']
 
 
+@admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'dataset']
 
 
+@admin.register(Network)
 class NetworkAdmin(admin.ModelAdmin):
     list_display = ['id', 'category', 'dataset']
     list_select_related = ['vector_data__dataset']
 
 
+@admin.register(NetworkEdge)
 class NetworkEdgeAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'network', 'dataset']
     list_select_related = ['network__vector_data__dataset']
 
 
+@admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'network', 'dataset']
     list_select_related = ['network__vector_data__dataset']
 
 
+@admin.register(TaskResult)
 class TaskResultAdmin(admin.ModelAdmin):
     list_display = ['id', 'task_type', 'inputs']
-
-
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Dataset, DatasetAdmin)
-admin.site.register(FileItem, FileItemAdmin)
-admin.site.register(Chart, ChartAdmin)
-admin.site.register(Layer, LayerAdmin)
-admin.site.register(LayerFrame, LayerFrameAdmin)
-admin.site.register(LayerStyle, LayerStyleAdmin)
-admin.site.register(Colormap, ColormapAdmin)
-admin.site.register(ColorConfig, ColorConfigAdmin)
-admin.site.register(ColormapConfig, ColormapConfigAdmin)
-admin.site.register(SizeConfig, SizeConfigAdmin)
-admin.site.register(SizeRangeConfig, SizeRangeConfigAdmin)
-admin.site.register(FilterConfig, FilterConfigAdmin)
-admin.site.register(RasterData, RasterDataAdmin)
-admin.site.register(VectorData, VectorDataAdmin)
-admin.site.register(VectorFeature, VectorFeatureAdmin)
-admin.site.register(Region, RegionAdmin)
-admin.site.register(Network, NetworkAdmin)
-admin.site.register(NetworkNode, NetworkNodeAdmin)
-admin.site.register(NetworkEdge, NetworkEdgeAdmin)
-admin.site.register(TaskResult, TaskResultAdmin)
